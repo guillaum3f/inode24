d961e55e88f84080ce25862930656c1d36cfd0ae
Guillaume Fe <guillaume.ferron@gmail.com>
2016-10-10 11:09:17 +0200

    fixed appserv

--- routes.js
+++ routes.js
@@ -1,4 +1,8 @@
 const passport = require('passport');
+const httpProxy = require('http-proxy');
+var proxy = httpProxy.createProxyServer({
+    //proxyTimeout : 3000
+});
 
 module.exports = function(app,config,scripts) {
 
@@ -21,14 +25,31 @@ module.exports = function(app,config,scripts) {
     });
 
     app.post('/login', scripts.retrieve_UID, passport.authenticate('ldapauth', {session: false}), function(req, res) {
-        res.send('you are logged');
+        req.session.logged = true;
+        res.redirect('/online');
+    });
+
+    app.get('/logout', function(req, res) {
+        req.session.logged = false;
+        res.redirect('/#');
+    });
+
+    app.get('/*', function(req, res) {
+        if(req.url === '/online') req.url='';
+        if(req.session.logged === true) {
+            proxy.web(req, res, { target: 'http://127.0.0.1:10102' });
+        } else {
+            res.redirect('/#login');
+        }
     });
 
     app.post('/register', function(req, res) {
         scripts.register(req, config, function(answer) {
             if(answer === true) {
-                res.end(req.body.email+' has been successfully registered. Welcome!');
+                console.log(req.body.email+' has been successfully registered. Welcome!');
+                res.redirect('/#login');
             } else {
+                res.write('<script>setTimeout(function(){window.location = "/#register"},1000)</script>');
                 res.end('Sorry, '+req.body.email+' is not available');
             }
         });
