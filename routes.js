04fc7a6649227ba1d30aefed9355d63c029d16f1
Guillaume Ferron <guillaume.ferron@gmail.com>
2016-10-17 02:58:14 +0200

    add a classical hello world as route example

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 module.exports = function(app,config,scripts) {
 
-    app.get('/', function(req, res) {
-        res.send('It Works!');
+    app.get('/hello', function(req, res) {
+        res.send('Hello World!');
     });
 
 };
