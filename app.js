3394c9452cf1d6d55805ae1fc288a7a669d9b8e0
Guillaume Ferron <guillaume.ferron@gmail.com>
2016-10-17 02:07:43 +0200

    fixed various

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var net = require('net');
 var recursive = require('recursive-readdir');
 var express = require('express');
 var bodyParser = require('body-parser');
+var jsonfile = require('jsonfile')
 const exec = require('child_process').exec;
 
 var platform = {};
@@ -46,28 +47,43 @@ fs.access(routes_file, fs.F_OK, function(err) {
 });
 
 //Starts sub-services
-fs.access(services_dir, fs.F_OK, function(err) {
-    if (!err) {
-        fs.readdir(services_dir, function(err, items) {
-            for (var i=0; i<items.length; i++) {
-                platform.services.push(services_dir+'/'+items[i]);
-                fs.access(services_dir+'/'+items[i]+'/app.js', fs.F_OK, function(err) {
-                    if (!err) {
-                        var item = platform.services.shift();
-                        exec('npm install --prefix '+item+' && node '+item+'/app.js', (error, stdout, stderr) => {
-                            if(error) console.log(error);
-                        });
-                    }
-                });
-            }
-        });
-    }
-});
+if(platform.config.services) {
+    fs.access(services_dir, fs.F_OK, function(err) {
+        if (!err) {
+            fs.readdir(services_dir, function(err, items) {
+                for (var i=0; i<items.length; i++) {
+                    platform.services.push(services_dir+'/'+items[i]);
+                    fs.access(services_dir+'/'+items[i]+'/app.js', fs.F_OK, function(err) {
+                        if (!err) {
+                            var item = platform.services.shift();
+                            exec('npm install --prefix '+item+' && node '+item+'/app.js', (error, stdout, stderr) => {
+                                if(error) console.log(error);
+                            });
+                        }
+                    });
+                }
+            });
+        }
+    });
+}
 
 //Enable static content
 app.use(express.static(static_dir)); //serve a static app
 
 
 //Start the server
-app.listen(platform.config.port);  //listen
-console.log('started '+ platform.config.port);
+fs.access(__dirname+'/../../config.json', fs.F_OK, function(err) {
+    if (!err) {
+        var _config = require(__dirname+'/../../config.json');
+        if(_config.services && _config.services[platform.config.name]) {
+            platform.config.port = _config.services[platform.config.name].split(':')[1];
+            jsonfile.writeFile(__dirname+'/config.json', platform.config, {spaces: 2}, function(err) {
+                if(err) console.error(err)
+            })
+        }
+    }
+
+    app.listen(platform.config.port);  //listen
+    console.log('started '+ platform.config.port);
+   
+});
