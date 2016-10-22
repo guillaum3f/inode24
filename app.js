3639338e9e34c509a504652324f7b292112d94a6
Guillaume Ferron <guillaume.ferron@gmail.com>
2016-10-22 19:23:32 +0200

    various

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ const exec = require('child_process').exec;
 const os = require('os');
 const dns = require('dns');
 const json = require('format-json');
+const Console = require('console').Console;
+const colors = require('colors')
+const figlet = require('figlet');
 
 var platform = {};
 platform.config = require('./config.json');
@@ -28,17 +31,41 @@ var routes_dir = __dirname+'/routes';
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
-//Informations on host
-console.log('FREEMEMORY ...'+os.freemem());
-console.log('HOMEDIR ...' + os.homedir());
-console.log('HOSTNAME ...' + os.hostname());
-console.log('NETWORK-INTERFACES ...' + json.plain(os.networkInterfaces()));
-console.log('ARCH ...'+os.arch());
-//console.log('CONSTANT ...'+os.constant());
-console.log('PLATFORM ...'+os.platform()+'#'+os.release());
-console.log('TMPDIR ...'+os.tmpdir());
-console.log('UPTIME ...'+os.uptime());
-console.log('CURRENT DNS ...'+dns.getServers());
+//Logs
+const logStream = fs.createWriteStream(__dirname+'/system/'+platform.config.name+'.log');
+const logger = new Console(logStream,logStream);
+const log = function() {
+    logger.log('['+new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')+']',colors.green.apply(true,arguments));
+}
+const warn = function() {
+    logger.log('['+new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')+']',colors.yellow.apply(true,arguments));
+}
+const error = function() {
+    logger.log('['+new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')+']',colors.red.apply(true,arguments));
+}
+
+figlet('Host config', function(err, ascii) {
+    if (err) {
+        log('Something went wrong...');
+        error(err);
+        return;
+    }
+
+    console.log(ascii)
+
+    //Informations on host
+    console.log(colors.white.bold('FREEMEMORY ...'+os.freemem()));
+    console.log(colors.white.bold('HOMEDIR ...' + os.homedir()));
+    console.log(colors.white.bold('HOSTNAME ...' + os.hostname()));
+    console.log(colors.white.bold('NETWORK-INTERFACES ...' + json.plain(os.networkInterfaces())));
+    console.log(colors.white.bold('ARCH ...'+os.arch()));
+    //console.log(colors.white.bold('CONSTANTS ...'+ json.plain(os.constants)));
+    console.log(colors.white.bold('PLATFORM ...'+os.platform()+'#'+os.release()));
+    console.log(colors.white.bold('TMPDIR ...'+os.tmpdir()));
+    console.log(colors.white.bold('UPTIME ...'+os.uptime()));
+    console.log(colors.white.bold('CURRENT DNS ...'+dns.getServers()));
+});
+
 
 //Require user middlewares
 fs.access(middlewares_dir, fs.F_OK, function(err) {
@@ -48,21 +75,15 @@ fs.access(middlewares_dir, fs.F_OK, function(err) {
                 if(path.extname(items[i]) === '.js') {
                     try {
                         platform.middlewares[path.basename(items[i],'.js')] = require(middlewares_dir+'/'+items[i]);
+                        log('Success [require middleware "'+items[i]+'"] ');
                     } catch (e) {
-                        console.log(e);
+                        error('Failure [require middleware "'+items[i]+'"] '+error);
                     }
                 }
             }
         });
     }
 });
-
-//Require routes
-//fs.access(routes_file, fs.F_OK, function(err) {
-//    if (!err) {
-//        require(routes_file)(app,platform.config,platform.middlewares);
-//    }
-//});
 
 //Require routes
 fs.access(routes_dir, fs.F_OK, function(err) {
@@ -72,8 +93,9 @@ fs.access(routes_dir, fs.F_OK, function(err) {
                 if(path.extname(items[i]) === '.js') {
                     try {
                         require(routes_dir+'/'+items[i])(app,platform.config,platform.middlewares);
+                        log('Success [require route "'+items[i]+'"] ');
                     } catch (e) {
-                        console.log(e);
+                        error('Failure [require route "'+items[i]+'"] '+error);
                     }
                 }
             }
@@ -88,8 +110,9 @@ if(platform.config && platform.config['third-part-servers']) {
             fs.readdir(third_part_servers_dir, function(err, items) {
                 for (var i=0; i<items.length; i++) {
                     if(platform.config['third-part-servers'].indexOf(items[i]) > -1){
+                        log('Launching third-part server "'+items[i]+'"');
                         exec('node '+third_part_servers_dir+'/'+items[i], (error, stdout, stderr) => {
-                            if(error) console.log(error);
+                            if(error) error('Failure [exec third-part server "'+items[i],+'"] '+error);
                         });
                     }
                 }
@@ -109,10 +132,10 @@ if(platform.config && platform.config.servers) {
                         fs.access(servers_dir+'/'+items[i]+'/app.js', fs.F_OK, function(err) {
                             if (!err) {
                                 var item = platform.servers.shift();
-                                var cmd = 'npm install --prefix '+item+' && node '+item+'/app.js&';
+                                log('Launching sub-server "'+items+'"');
+                                var cmd = 'cd '+servers_dir+'/'+item+' && npm install && node app.js&';
                                 exec(cmd, (error, stdout, stderr) => {
-                                console.log('oui');
-                                    if(error) console.log(error);
+                                    if(error) error('Failure [exec sub-server '+items[i],+']"] '+error);
                                 });
                             }
                         });
@@ -125,6 +148,18 @@ if(platform.config && platform.config.servers) {
 
 //Enable static content
 app.use(express.static(static_dir)); //serve a static app
+log("Static content is served from "+static_dir);
+figlet('Static Content', function(err, ascii) {
+    if (err) {
+        log('Something went wrong...');
+        error(err);
+        return;
+    }
+
+    console.log(ascii)
+    console.log(colors.yellow("Static content is served from "+static_dir));
+});
+
 
 
 //Start the server
@@ -134,12 +169,23 @@ fs.access(__dirname+'/../../config.json', fs.F_OK, function(err) {
         if(_config.servers && _config.servers[platform.config.name]) {
             platform.config.port = _config.servers[platform.config.name].split(':')[1];
             jsonfile.writeFile(__dirname+'/config.json', platform.config, {spaces: 2}, function(err) {
-                if(err) console.error(err)
+                if(err) err('Failure [write config]"] '+err);
             })
         }
     }
 
     app.listen(platform.config.port);  //listen
-    console.log('started '+ platform.config.port);
+    log('Started inode "'+platform.config.name+'" at address localhost:'+ platform.config.port);
+    figlet('Inode : '+platform.config.name, function(err, ascii) {
+        if (err) {
+            log('Something went wrong...');
+            error(err);
+            return;
+        }
+
+        console.log(ascii)
+        console.log(colors.green('Started inode "'+platform.config.name+'" at address localhost:'+ platform.config.port));
+
+    });
    
 });
